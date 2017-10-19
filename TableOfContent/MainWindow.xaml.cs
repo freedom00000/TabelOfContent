e2b3dbdae228f4d6e751fd56cc48077d83a907f6
desperado <qblr11@gmail.com>
2017-10-19 15:29:13 +0300

    one toc

--- TableOfContent/MainWindow.xaml.cs
+++ TableOfContent/MainWindow.xaml.cs
@@ -28,16 +28,15 @@ namespace TableOfContent
         {
   
             using (WordprocessingDocument docWord = WordprocessingDocument.Open(filepath,true)) {
-             //   var doc = docWord.MainDocumentPart.Document;
-                XElement firstPara = 
-                     GetXDocument(docWord.MainDocumentPart)
-                    .Descendants(w + "p")
-                    .FirstOrDefault();
-                AddToc(docWord, firstPara,
-@"TOC \o '1-3' \h \z \u", "Оглавление", null);
-                //OpenXmlElement block = doc.Descendants<DocPartGallery>().
-                //          Where(b => b.Val.HasValue &&
-                //            (b.Val.Value == "Table of Contents")).FirstOrDefault();
+
+                    XElement firstPara =
+                         GetXDocument(docWord.MainDocumentPart)
+                        .Descendants(w + "p")
+                        .FirstOrDefault();
+                if(firstPara.Parent.Name.LocalName != "sdtContent")
+                    AddToc(docWord, firstPara,
+    @"TOC \o '1-3' \h \z \u", "Оглавление", null);
+
 
             } 
         }
@@ -45,9 +44,6 @@ namespace TableOfContent
 
         public static void AddToc(WordprocessingDocument doc, XElement addBefore, string switches, string title, int? rightTabPos)
         {
-            // UpdateFontTablePart(doc);
-            // UpdateStylesPartForToc(doc);
-            //UpdateStylesWithEffectsPartForToc(doc);
 
             if (title == null)
                 title = "Оглавsdaweление";
@@ -121,17 +117,6 @@ namespace TableOfContent
             addBefore.AddBeforeSelf(sdt);
             PutXDocument(doc.MainDocumentPart);
 
-            //XDocument settingsXDoc = doc.MainDocumentPart.DocumentSettingsPart.GetXDocument();
-            //XElement updateFields = settingsXDoc.Descendants(W.updateFields).FirstOrDefault();
-            //if (updateFields != null)
-            //    updateFields.Attribute(W.val).Value = "true";
-            //else
-            //{
-            //    updateFields = new XElement(W.updateFields,
-            //        new XAttribute(W.val, "true"));
-            //    settingsXDoc.Root.Add(updateFields);
-            //}
-            //doc.MainDocumentPart.DocumentSettingsPart.PutXDocument();
         }
 
         public static XDocument GetXDocument(OpenXmlPart part)
@@ -158,157 +143,6 @@ namespace TableOfContent
             }
         }
 
-        //void UpdateFontTablePart(WordprocessingDocument doc)
-        //{
-        //    FontTablePart fontTablePart = doc.MainDocumentPart.FontTablePart;
-
-        //    XDocument fontTableXDoc = GetXDocument(fontTablePart);
-
-        //    AddElementIfMissing(fontTableXDoc,
-        //        fontTableXDoc
-        //            .Root
-        //            .Elements(w + "font")
-        //            .Where(e => (string)e.Attribute(w + "name") == "Tahoma")
-        //            .FirstOrDefault(),
-        //        @"<w:font w:name='Tahoma' xmlns:w='http://schemas.openxmlformats.org/wordprocessingml/2006/main'>
-        //             <w:panose1 w:val='020B0604030504040204'/>
-        //             <w:charset w:val='00'/>
-        //             <w:family w:val='swiss'/>
-        //             <w:pitch w:val='variable'/>
-        //             <w:sig w:usb0='E1002EFF' w:usb1='C000605B' w:usb2='00000029' w:usb3='00000000' w:csb0='000101FF' w:csb1='00000000'/>
-        //           </w:font>");
-
-        //    PutXDocument(fontTablePart);
-        //}
-
-        //private  void AddElementIfMissing(XDocument partXDoc, XElement existing, string newElement)
-        //{
-        //    if (existing != null)
-        //        return;
-        //    XElement newXElement = XElement.Parse(newElement);
-        //    newXElement.Attributes().Where(a => a.IsNamespaceDeclaration).Remove();
-        //    partXDoc.Root.Add(newXElement);
-        //}
-
-        //private  void UpdateStylesPartForToc(WordprocessingDocument doc)
-        //{
-        //    StylesPart stylesPart = doc.MainDocumentPart.StyleDefinitionsPart;
-
-        //    XDocument stylesXDoc = GetXDocument(stylesPart);
-        //    UpdateAStylePartForToc(stylesXDoc);
-        //    PutXDocument(stylesPart);
-        //}
-
-        //private  void UpdateStylesWithEffectsPartForToc(WordprocessingDocument doc)
-        //{
-        //    StylesWithEffectsPart stylesWithEffectsPart = doc.MainDocumentPart.StylesWithEffectsPart;
-
-        //    XDocument stylesWithEffectsXDoc = GetXDocument(stylesWithEffectsPart);
-        //    UpdateAStylePartForToc(stylesWithEffectsXDoc);
-        //    PutXDocument(stylesWithEffectsPart);
-        //}
-
-        //private  void UpdateAStylePartForToc(XDocument partXDoc)
-        //{
-        //    AddElementIfMissing(
-        //        partXDoc,
-        //        partXDoc.Root.Elements(w + "style")
-        //            .Where(e => (string)e.Attribute(w + "type") == "paragraph" && (string)e.Attribute(w + "styleId") == "TOCHeading")
-        //            .FirstOrDefault(),
-        //        @"<w:style w:type='paragraph' w:styleId='TOCHeading' xmlns:w='http://schemas.openxmlformats.org/wordprocessingml/2006/main'>
-        //            <w:name w:val='TOC Heading'/>
-        //            <w:basedOn w:val='Heading1'/>
-        //            <w:next w:val='Normal'/>
-        //            <w:uiPriority w:val='39'/>
-        //            <w:semiHidden/>
-        //            <w:unhideWhenUsed/>
-        //            <w:qFormat/>
-        //            <w:pPr>
-        //              <w:outlineLvl w:val='9'/>
-        //            </w:pPr>
-        //            <w:rPr>
-        //              <w:lang w:eastAsia='ja-JP'/>
-        //            </w:rPr>
-        //          </w:style>");
-
-        //    for (int i = 1; i <= 6; ++i)
-        //    {
-        //        AddElementIfMissing(
-        //            partXDoc,
-        //            partXDoc.Root.Elements(w + "style")
-        //                .Where(e => (string)e.Attribute(w + "type") == "paragraph" && (string)e.Attribute(w + "styleId") == ("TOC" + i.ToString()))
-        //                .FirstOrDefault(),
-        //            String.Format(
-        //                @"<w:style w:type='paragraph' w:styleId='TOC{0}' xmlns:w='http://schemas.openxmlformats.org/wordprocessingml/2006/main'>
-        //                    <w:name w:val='toc {0}'/>
-        //                    <w:basedOn w:val='Normal'/>
-        //                    <w:next w:val='Normal'/>
-        //                    <w:autoRedefine/>
-        //                    <w:uiPriority w:val='39'/>
-        //                    <w:unhideWhenUsed/>
-        //                    <w:pPr>
-        //                      <w:spacing w:after='100'/>
-        //                    </w:pPr>
-        //                  </w:style>", i));
-        //    }
-
-        //    AddElementIfMissing(
-        //        partXDoc,
-        //        partXDoc.Root.Elements(w + "style")
-        //            .Where(e => (string)e.Attribute(w + "type") == "character" && (string)e.Attribute(w + "styleId") == "Hyperlink")
-        //            .FirstOrDefault(),
-        //        @"<w:style w:type='character' w:styleId='Hyperlink' xmlns:w='http://schemas.openxmlformats.org/wordprocessingml/2006/main'>
-        //             <w:name w:val='Hyperlink'/>
-        //             <w:basedOn w:val='DefaultParagraphFont'/>
-        //             <w:uiPriority w:val='99'/>
-        //             <w:unhideWhenUsed/>
-        //             <w:rPr>
-        //               <w:color w:val='0000FF' w:themeColor='hyperlink'/>
-        //               <w:u w:val='single'/>
-        //             </w:rPr>
-        //           </w:style>");
-
-        //    AddElementIfMissing(
-        //        partXDoc,
-        //        partXDoc.Root.Elements(w + "style")
-        //            .Where(e => (string)e.Attribute(w + "type") == "paragraph" && (string)e.Attribute(w + "styleId") == "BalloonText")
-        //            .FirstOrDefault(),
-        //        @"<w:style w:type='paragraph' w:styleId='BalloonText' xmlns:w='http://schemas.openxmlformats.org/wordprocessingml/2006/main'>
-        //            <w:name w:val='Balloon Text'/>
-        //            <w:basedOn w:val='Normal'/>
-        //            <w:link w:val='BalloonTextChar'/>
-        //            <w:uiPriority w:val='99'/>
-        //            <w:semiHidden/>
-        //            <w:unhideWhenUsed/>
-        //            <w:pPr>
-        //              <w:spacing w:after='0' w:line='240' w:lineRule='auto'/>
-        //            </w:pPr>
-        //            <w:rPr>
-        //              <w:rFonts w:ascii='Tahoma' w:hAnsi='Tahoma' w:cs='Tahoma'/>
-        //              <w:sz w:val='16'/>
-        //              <w:szCs w:val='16'/>
-        //            </w:rPr>
-        //          </w:style>");
-
-        //    AddElementIfMissing(
-        //        partXDoc,
-        //        partXDoc.Root.Elements(w + "style")
-        //            .Where(e => (string)e.Attribute(w + "type") == "character" &&
-        //                (bool?)e.Attribute(w + "customStyle") == true && (string)e.Attribute(w + "styleId") == "BalloonTextChar")
-        //            .FirstOrDefault(),
-        //        @"<w:style w:type='character' w:customStyle='1' w:styleId='BalloonTextChar' xmlns:w='http://schemas.openxmlformats.org/wordprocessingml/2006/main'>
-        //            <w:name w:val='Balloon Text Char'/>
-        //            <w:basedOn w:val='DefaultParagraphFont'/>
-        //            <w:link w:val='BalloonText'/>
-        //            <w:uiPriority w:val='99'/>
-        //            <w:semiHidden/>
-        //            <w:rPr>
-        //              <w:rFonts w:ascii='Tahoma' w:hAnsi='Tahoma' w:cs='Tahoma'/>
-        //              <w:sz w:val='16'/>
-        //              <w:szCs w:val='16'/>
-        //            </w:rPr>
-        //          </w:style>");
-        //}
     }
 
   
